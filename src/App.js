90b4fd6e095b0541c4ec28b6fe052f7b463cf02d
Abraham Soto <abrahamsc1980@gmail.com>
2021-01-20 09:37:33 -0500

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
 
+function Header(props) {
+  return (
+    <header>
+      <h1>{props.name}'s Kitchen</h1>
+    </header>
+  );
+}
+
+function Main (props) {
+  return (
+    <section>
+      <p>We serve the most {props.adjective} food around</p>
+      <ul style={{textAlign: 'left'}}>
+        {props.dishes.map((dish) => (<li>{dish}</li>))}
+      </ul>
+    </section>
+  )
+}
+
+function Footer (props) {
+  return (
+    <h3>Copyright {props.year}</h3>
+  )
+} 
+
+const dishes = [
+  "Mac&Cheese",
+  "Salmon",
+  "Ribeye",
+  "Minestroni"
+]
+
+dishes.map((dish) => console.log(dish));
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header name="Abraham"/>
+      <Main adjective="amazing" dishes={dishes}/>
+      <Footer year={new Date().getFullYear()}/>
     </div>
   );
 }
